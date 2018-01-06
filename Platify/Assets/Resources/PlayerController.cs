9cedb0a6247fd739ac4b2ef683f194891526fe36
Benjamin B <bennibryne@gmail.com>
2018-01-06 01:56:48 +0100

    Imported Platify
    Attempted import of Platify prototype

---
+++ Platify/Assets/Resources/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public int speed;
+    bool jumping;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        if (jumping)
+        {
+            rb2d.AddForce(Vector2.up * 4.5f, ForceMode2D.Impulse);
+            jumping = false;
+        }
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
+        rb2d.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Respawn"))
+        {
+            transform.position = new Vector2(-3, -1);
+        }
+    }
+}
