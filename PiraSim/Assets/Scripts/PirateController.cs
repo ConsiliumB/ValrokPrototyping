bd9f6a48a6b709ee55ca877ae4f773fd98d393ff
Benjamin B <bennibryne@gmail.com>
2018-01-12 12:04:34 +0100

    Added PiraSim v1
    Imported PiraSim v1 prototype

---
+++ PiraSim/Assets/Scripts/PirateController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateController : MonoBehaviour {
+
+    public Sprite healthy;
+    public Sprite damaged;
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+        {
+            spriteRenderer.sprite = healthy;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            if (spriteRenderer.sprite.Equals(healthy))
+            {
+                spriteRenderer.sprite = damaged;
+            } else
+            {
+                Destroy(gameObject);
+            }            
+        }
+    }
+}
---
+++ PiraSim/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public int speed;
+    public GameObject projectile;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float vmov = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(0, vmov);
+        rb2d.AddForce(movement * speed);
+    }
+
+    private void Shoot()
+    {
+        var bullet = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = Vector2.left * 10;
+
+        Destroy(bullet, 2.0f);
+    }
+}
