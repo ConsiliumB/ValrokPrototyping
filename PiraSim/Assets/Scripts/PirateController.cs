b6e6fe2466c5005ce7745c737238c423d3e45376
Benjamin B <bennibryne@gmail.com>
2018-01-15 15:40:26 +0100

    Added simple AI to PiraSim
    Implemented AI State changes based on proximity to player

--- PiraSim/Assets/Scripts/PirateController.cs
+++ PiraSim/Assets/Scripts/PirateController.cs
@@ -6,43 +6,133 @@ using UnityEngine;
 public class PirateController : MonoBehaviour {
     public float rotationSpeed;
     public float speed;
-    public Sprite healthy;
-    public Sprite damaged;
-    private SpriteRenderer spriteRenderer;
-    private Rigidbody2D rb2d;
+    public float chaseThreshold;
+    public float fightThreshold;
+    public GameObject player;
+    public GameObject projectile;
 
-	// Use this for initialization
+
+
+    private Rigidbody2D rb2d;
+    private Animator animator;
+
+    private int currentHealth;
+    private bool isDead;
+    private State state;
+
+    private enum State
+    {
+        Patrol, Chase, Flee, Fight, Dead
+    }
+
 	void Start () {
+        state = State.Patrol;
+        currentHealth = 3;
         rb2d = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        if (spriteRenderer.sprite == null)
+        animator = GetComponent<Animator>();
+	}
+
+    private void Update()
+    {
+        if (currentHealth < 1)
         {
-            spriteRenderer.sprite = healthy;
+            state = State.Dead;
+            Destroy(gameObject, 1.0f);
+            return;
+        }
+
+        var distance = Vector2.Distance(transform.position, player.transform.position);
+        if (distance < fightThreshold)
+        {
+            if (state != State.Fight)
+            {
+                state = State.Fight;
+                InvokeRepeating("ShootLeft", 0.1f, 0.5f);
+                //InvokeRepeating("ShootRight", 0.1f, 0.5f);
+            }
+     
+        }
+        else if (distance < chaseThreshold)
+        {
+            if (state != State.Chase)
+            {
+                state = State.Chase;
+                CancelInvoke();
+            }
+        }
+    }
+
+    void FixedUpdate () {
+        switch (state)
+        {
+            case State.Patrol:
+                CirclePatrol();
+                break;
+            case State.Chase:
+                Chase();
+                break;
+            case State.Flee:
+                Flee();
+                break;
+            case State.Fight:
+                Fight();
+                break;
+            case State.Dead:
+                rb2d.velocity *= 0;
+                break;
+            default:
+                CirclePatrol();
+                break;
         }
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        CirclePatrol();
-	}
-
     private void CirclePatrol()
     {
         rb2d.MoveRotation(rb2d.rotation + rotationSpeed * Time.fixedDeltaTime);
         rb2d.velocity = transform.up * speed * -1;
     }
 
+    private void Chase()
+    {
+
+        //rb2d.MoveRotation(-1 * Vector3.SignedAngle(player.transform.position - transform.position, Vector3.down, Vector3.forward));
+        rb2d.MoveRotation(-1 * Vector3.SignedAngle(player.transform.position - transform.position, Vector3.down, Vector3.forward));
+        rb2d.velocity = transform.up * speed * -1;
+    }
+
+    private void Flee()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Fight()
+    {
+        rb2d.MoveRotation(-1 * Vector3.SignedAngle(player.transform.position - transform.position, Vector3.left, Vector3.forward));
+        rb2d.velocity *= 0;
+    }
+
+    private void ShootLeft()
+    {
+        var bullet = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * -1 * 10;
+
+        Destroy(bullet, 2.0f);
+    }
+
+    private void ShootRight()
+    {
+        var bullet = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * 10;
+
+        Destroy(bullet, 2.0f);
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Projectile"))
         {
-            if (spriteRenderer.sprite.Equals(healthy))
-            {
-                spriteRenderer.sprite = damaged;
-            } else
-            {
-                Destroy(gameObject);
-            }            
+            currentHealth -= 1;
+            animator.SetInteger("health", currentHealth);
         }
     }
 }
--- PiraSim/Assets/Scripts/PlayerController.cs
+++ PiraSim/Assets/Scripts/PlayerController.cs
@@ -5,34 +5,75 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public int speed;
     public GameObject projectile;
+    public int currentHealth;
+
     private Rigidbody2D rb2d;
+    private Animator animator;
+    private bool isDead;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        if (currentHealth < 1)
+        {
+            isDead = true;
+            return;
+        }
         if (Input.GetButtonDown("Jump"))
         {
-            Shoot();
+            ShootLeft();
+            ShootRight();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ShootLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ShootRight();
         }
     }
 
     private void FixedUpdate()
     {
+        if (isDead)
+        {
+            rb2d.velocity *= 0;
+            return;
+        }
         float vmov = Input.GetAxis("Vertical");
         float hmov = Input.GetAxis("Horizontal");
         rb2d.MoveRotation(rb2d.rotation - hmov * speed * 5 * Time.fixedDeltaTime);
         rb2d.velocity = transform.up * vmov * speed / 2;
     }
 
-    private void Shoot()
+    private void ShootLeft()
     {
         var bullet = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
         bullet.GetComponent<Rigidbody2D>().velocity = transform.right * -1 * 10;
 
         Destroy(bullet, 2.0f);
+    }
+
+    private void ShootRight()
+    {
+        var bullet = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.right * 10;
+
+        Destroy(bullet, 2.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyProjectile"))
+        {
+            currentHealth -= 1;
+            animator.SetInteger("health", currentHealth);
+        }
     }
 }
