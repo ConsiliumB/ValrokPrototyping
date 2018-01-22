a776e616c1c53c6b262c1c0489a5d88b7e02d15a
Persijn <persijn.d.k@gmail.com>
2018-01-22 12:50:57 +0100

    Iso prototype Persijn

---
+++ Isometric_art_prototype/Assets/Scripts/Player_move_iso.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move_iso : MonoBehaviour {
+
+    public float speed;
+    private float speedY;
+
+	// Use this for initialization
+	void Start () {
+        speedY = speed / 1.7f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var inputX = Input.GetAxisRaw("Horizontal");
+        var inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 pos = transform.position;
+        //RIGHT && UP
+        if (inputX > 0 && inputY > 0)
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        //RIGHT DOWN
+        else if (inputX > 0 && inputY < 0)
+        {
+            pos.y -= speed * Time.deltaTime; 
+        }
+        //LEFT UP
+        else if (inputX < 0 && inputY > 0)
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        //LEFT DOWN
+        else if (inputX < 0 && inputY < 0)
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        else if (inputX > 0)
+        {
+            pos.x += speed * Time.deltaTime;
+            pos.y -= speedY * Time.deltaTime;
+        }
+        else if (inputY > 0)
+        {
+            pos.x += speed * Time.deltaTime;
+            pos.y += speedY * Time.deltaTime;
+        }
+        else if (inputX < 0)
+        {
+            pos.x -= speed * Time.deltaTime;
+            pos.y += speedY * Time.deltaTime;
+        }
+        else if (inputY < 0)
+        {
+            pos.x -= speed * Time.deltaTime;
+            pos.y -= speedY * Time.deltaTime;
+        }
+
+        transform.position = pos;
+	}
+}
---
+++ Isometric_art_prototype/Assets/Scripts/Player_move_normal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move_normal : MonoBehaviour {
+
+    public float speed;
+    public Sprite up;
+    public Sprite down;
+    public Sprite left;
+    public Sprite right;
+    private SpriteRenderer srend;
+
+	// Use this for initialization
+	void Start () {
+        srend = gameObject.GetComponentInChildren<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var inputX = Input.GetAxisRaw("Horizontal");
+        var inputY = Input.GetAxisRaw("Vertical");
+
+        var pos = transform.position;
+        pos.x += inputX * speed * Time.deltaTime;
+        pos.y += inputY * speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (inputX > 0)
+        {
+            srend.sprite = left;
+        }
+        else if (inputX < 0)
+        {
+            srend.sprite = right;
+        }
+        else if (inputY > 0)
+        {
+            srend.sprite = up;
+        }
+        else if (inputY < 0)
+        {
+            srend.sprite = down;
+        }
+    }
+}
