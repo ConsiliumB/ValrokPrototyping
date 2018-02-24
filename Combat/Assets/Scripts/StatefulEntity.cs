a9bafa74c6fdb60520e5bcbbad2d27f13a4c14d1
Benjamin B <bennibryne@gmail.com>
2018-02-24 16:49:34 +0100

    Base class for State and StatefulEntity created
    These will be used in every entity that needs states, which is probably all of them to be honest.

---
+++ Combat/Assets/Scripts/StatefulEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatefulEntity : MonoBehaviour {
+
+    public State currentState;
+
+	private void Update () {
+        currentState.Execute();
+	}
+
+    public void ChangeState(State newState)
+    {
+        if (currentState != null)
+        {
+            currentState.FinishState();
+        }
+
+        currentState = newState;
+
+        if (currentState != null)
+        {
+            currentState.PrepareState();
+        }
+    }
+}
