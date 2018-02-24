f0abab503e0ea2a6d6cf1f4106f0f5f06fc3a147
Benjamin B <bennibryne@gmail.com>
2018-02-24 16:49:34 +0100

    Base class for State and StatefulEntity created
    These will be used in every entity that needs states, which is probably all of them to be honest.

---
+++ Combat/Assets/Scripts/State.cs
@@ -0,0 +1,27 @@
+﻿public abstract class State
+{
+
+    //Reference to parent entity
+    protected StatefulEntity Entity { get; private set; }
+
+    //Private empty constructor to avoid a State being initialized without a parent entity
+    private State() { }
+
+    //Save a reference to the parent entity
+    public State(StatefulEntity entity)
+    {
+        Entity = entity;
+    }
+
+    //This method be called when transitioning into this state,
+    //and will contain any one-time set-up and initialization needed for this state.
+    public virtual void PrepareState() { }
+
+    //This method will execute this states behaviour.
+    //Should be called on every update in parent entity
+    public abstract void Execute();
+
+    //This method be called when transitioning out of this state,
+    //and will contain any cleanup or last actions needed to finish this state
+    public virtual void FinishState() { }
+}
