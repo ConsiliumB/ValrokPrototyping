71e7f269c34b93c2c84aeae68fc9334599cde948
Benjamin B <bennibryne@gmail.com>
2018-03-12 11:29:02 +0100

    Depth + graphics = love

---
+++ Combat/Assets/Scripts/Rendering/DynamicDepth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicDepth : MonoBehaviour
+{
+    private SpriteRenderer Renderer;
+    public float offsetToActualBase;
+
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+        Renderer.sortingOrder = (int)((transform.position.y + offsetToActualBase) * -100f);
+    }
+
+    private void Update()
+    {
+        Renderer.sortingOrder = (int)((transform.position.y + offsetToActualBase) * -100f);
+    }
+}
---
+++ Combat/Assets/Scripts/Rendering/StaticDepth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticDepth : MonoBehaviour
+{
+    private SpriteRenderer Renderer;
+    public float offsetToActualBase;
+
+    // Use this for initialization
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+        Renderer.sortingOrder = (int)((transform.position.y + offsetToActualBase) * -100f);
+    }
+}
