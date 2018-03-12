a6c6e5fbb9c4377271e217d375d06fcf3b7d04b9
Benjamin B <bennibryne@gmail.com>
2018-03-12 15:44:07 +0100

    Creating Shade enemy that hunts closest and has range attack
    WIP

---
+++ Combat/Assets/Scripts/Enemies/ShadeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadeController : StatefulEntity {
+
+    //public Movement Movement { get; private set; }
+
+    // Use this for initialization
+    void Start () {
+        ChangeState(new ChaseNearestState(this));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class ChaseNearestState : State
+{
+    private CompanionController Companion;
+    private PlayerController Player;
+    private ShadeController Shade;
+
+    private Vector2 playerHeading;
+    private Vector2 companionHeading;
+
+    public ChaseNearestState(ShadeController shade)
+    {
+        Shade = shade;
+    }
+
+    public override void PrepareState()
+    {
+        Player = PlayerController.Instance;
+        Companion = CompanionController.Instance;
+    }
+
+    public override void Execute()
+    {
+        companionHeading = Companion.transform.position - Shade.transform.position;
+        playerHeading = Player.transform.position - Shade.transform.position;
+        if (companionHeading.sqrMagnitude < playerHeading.sqrMagnitude)
+        {
+            //movement?
+        }
+        else
+        {
+            //movement?
+        }
+    }
+
+    public override void FinishState()
+    {
+        base.FinishState();
+    }
+}
