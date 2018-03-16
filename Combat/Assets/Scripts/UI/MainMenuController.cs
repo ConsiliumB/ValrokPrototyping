cdd46cf44848cd7bc9f091bb205f43d7d548273e
Benjamin B <bennibryne@gmail.com>
2018-03-16 09:25:11 +0100

    Added main menu with loading screen, organized code
    Added some folders and moved some files

---
+++ Combat/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject mainMenuScreen;
+    public GameObject loadingScreen;
+    public GameObject optionsScreen;
+    public Slider loadingBar;
+    private AsyncOperation loader;
+
+    public void PlayGame() {
+        StartCoroutine("LoadGame");
+	}
+
+	public void Options() {
+		// Switch to Options Menu
+	}
+
+	public void QuitGame() {
+        Application.Quit();
+	}
+
+    IEnumerator LoadGame()
+    {
+        mainMenuScreen.SetActive(false);
+        loadingScreen.SetActive(true);
+        loader = SceneManager.LoadSceneAsync("MainScene");
+        while (!loader.isDone)
+        {
+            loadingBar.value = loader.progress;
+            yield return null;
+        }
+    }
+}
