97a9f349a6ba7acce58ee676688246b5360543fb
Benjamin B <bennibryne@gmail.com>
2018-03-19 21:49:18 +0100

    Added simple tile spawning, placeholder button and new fonts

---
+++ Combat/Assets/Scripts/UI/MenuBackgroundController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackgroundController : MonoBehaviour {
+
+    public GameObject tile;
+    private List<Coordinate> coordinates = new List<Coordinate>();
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("GenerateBackground");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator GenerateBackground()
+    {
+        var origin = new Coordinate(0, 0);
+        var directions = new Coordinate[] {
+            Coordinate.North,
+            Coordinate.North + Coordinate.East,
+            Coordinate.East,
+            Coordinate.East + Coordinate.South,
+            Coordinate.South,
+            Coordinate.South + Coordinate.West,
+            Coordinate.West,
+            Coordinate.West + Coordinate.North
+        };
+
+        Coordinate curr;
+        coordinates.Add(origin);
+
+        foreach (var item in directions)
+        {
+            coordinates.Add(origin + item);
+        }
+
+        foreach (var item in directions)
+        {
+            curr = origin + item * 2;
+            if (curr.X == origin.X)
+            {
+                if (curr.Y > 0)
+                {
+                    coordinates.Add(curr + Coordinate.West);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.East);
+                }
+                else
+                {
+                    coordinates.Add(curr + Coordinate.East);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.West);
+                }
+            }
+            else if (curr.Y == origin.Y)
+            {
+                if (curr.X > 0)
+                {
+                    coordinates.Add(curr + Coordinate.North);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.South);
+                }
+                else
+                {
+                    coordinates.Add(curr + Coordinate.South);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.North);
+                }
+
+            } else
+            {
+                coordinates.Add(curr);
+            }
+        }
+
+        foreach (var item in directions)
+        {
+            curr = origin + item * 3;
+            if (curr.X == origin.X)
+            {
+                if (curr.Y > 0)
+                {
+                    coordinates.Add(curr + Coordinate.West * 2);
+                    coordinates.Add(curr + Coordinate.West);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.East);
+                    coordinates.Add(curr + Coordinate.East * 2);
+                }
+                else
+                {
+                    coordinates.Add(curr + Coordinate.East * 2);
+                    coordinates.Add(curr + Coordinate.East);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.West);
+                    coordinates.Add(curr + Coordinate.West * 2);
+                }
+
+            }
+            else if (curr.Y == origin.Y)
+            {
+                if (curr.X > 0)
+                {
+                    coordinates.Add(curr + Coordinate.North * 2);
+                    coordinates.Add(curr + Coordinate.North);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.South);
+                    coordinates.Add(curr + Coordinate.South * 2);
+                }
+                else
+                {
+                    coordinates.Add(curr + Coordinate.South * 2);
+                    coordinates.Add(curr + Coordinate.South);
+                    coordinates.Add(curr);
+                    coordinates.Add(curr + Coordinate.North);
+                    coordinates.Add(curr + Coordinate.North * 2);
+                }
+
+            } else
+            {
+                coordinates.Add(curr);
+            }
+            yield return null;
+        }
+
+        StartCoroutine("InstantiateBackground");
+    }
+
+    IEnumerator InstantiateBackground()
+    {
+        foreach (Coordinate item in coordinates)
+        {
+            Instantiate(tile, WorldGen.MapToPixel(item), Quaternion.identity, transform);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
