7ceb9760cd51d1229a7e99ec795a109b5fe4374a
Benjamin B <bennibryne@gmail.com>
2018-04-05 14:23:37 +0200

    Laggy but functional transparency

---
+++ Combat/Assets/Scripts/Rendering/FoilageTransparency.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoilageTransparency : MonoBehaviour {
+    [Range(0,1)]
+    public float transparency;
+
+    private int overlappingObjects = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != gameObject.layer)
+        {
+            overlappingObjects++;
+
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, transparency);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != gameObject.layer)
+        {
+            overlappingObjects--;
+
+            if (overlappingObjects == 0)
+            {
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 1f);
+            }
+        }
+    }
+}
