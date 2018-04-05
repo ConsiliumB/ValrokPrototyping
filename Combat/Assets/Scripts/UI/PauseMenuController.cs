beedbdf64eb921cd1d9536383f9dd47ff3b695fb
Benjamin B <bennibryne@gmail.com>
2018-04-06 01:07:34 +0200

    Sleep overrated. Pause menu implemented.

---
+++ Combat/Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour {
+    public GameObject PauseMenu;
+    private bool paused = false;
+
+    private void Start()
+    {
+        Resume();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        paused = true;
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
